61cea2a0b31db19e44038e60b57b449a3716e348
Smile <xcxerxes@aliyun.com>
2019-01-31 23:49:50 +0800

    input 组件

--- docs/.vuepress/components/Demos/Input/Basic.vue
+++ docs/.vuepress/components/Demos/Input/Basic.vue
@@ -1,5 +1,22 @@
 <template>
-  <div>
-    <vf-input />
+  <div class="wrapper">
+    <vf-input class="input-item" placeholder="Basic use" />
+    <vf-input class="input-item"  placeholder="请输入..." v-model="value1" />
+    <vf-input class="input-item"  placeholder="请输入..." disabled />
   </div>
 </template>
+<script>
+export default {
+  data: () =>({
+    value1: ''
+  })
+}
+</script>
+<style lang="scss" scoped>
+.wrapper {
+  .input-item {
+    margin-bottom: 15px;
+  }
+}
+</style>
+
--- src/components/vfInput/vfInput.vue
+++ src/components/vfInput/vfInput.vue
@@ -2,17 +2,66 @@
   <div class="vf-input"
     :class="[`vf-input__${color}`]"
   >
-    <input type="text" placeholder="search" >
+    <input :type="$attrs.type || 'text'"
+      ref="input"
+      :value="value"
+      v-bind="$attrs"
+      :placeholder="null"
+      v-on="listeners"
+    >
+    <span v-if="!isHasValue" 
+      class="vf-input__placeholder"
+      @click="placeholderClick"
+    >{{$attrs.placeholder}}</span>
   </div>
 </template>
 <script>
 export default {
   name: 'VfInput',
   props: {
+    value: {
+      type: [String, Number],
+      default: ''
+    },
     color: {
       type: String,
       default: 'primary'
     }
+  },
+  data: () =>({
+    isHasValue: false
+  }),
+  computed: {
+    listeners() {
+      debugger
+      return {
+        ...this.$listeners,
+        
+        input: event => this.handleInput(event)
+      }
+    }
+  },
+  methods: {
+    handleInput(event) {
+      const {value} = event.target
+      this.$emit('input', event.target.value)
+      if (!this.cacheValue['cacheValue']) {
+        if (value) {
+          this.cacheValue.cacheValue = true
+          this.isHasValue = true
+        }
+      }
+      if (!value) {
+        this.cacheValue['cacheValue'] = false
+        this.isHasValue = false
+      }
+    },
+    placeholderClick() {
+      this.$refs.input.focus()
+    }
+  },
+  created() {
+    this.cacheValue = {}
   }
 }
 </script>
